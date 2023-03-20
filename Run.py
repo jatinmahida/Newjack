16445a4807d89a6abc422961b364ba3c6590d334
ktynski <kristin@frac.tl>
2023-03-20 19:21:25 -0400

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,10 +1,99 @@
 import streamlit as st
 import pandas as pd
 import concurrent.futures
+import openai
+import pandas as pd
+import numpy as np
+import requests
+import json
+import os
+from serpapi import GoogleSearch
+from newspaper import Article
+import nltk
+import concurrent.futures
 
-# Import your existing functions and variables from your original script
-from getdata import get_google_news_data, download_and_parse_article, generate_summary, generate_content
+nltk.download('punkt')
 
+serpapi_key = 'enter your serpapi key'
+openai_key = 'enter your openai key'
+
+
+
+def download_and_parse_article(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
+
+
+
+
+def get_google_news_data(query, num_results=10, api_key=None):
+    params = {
+        "api_key": api_key if api_key else serpapi_key,
+        "engine": "google",
+        "q": query,
+        "tbm": "nws",
+        "num": num_results
+    }
+    response = requests.get('https://serpapi.com/search.json', params=params)
+    data = json.loads(response.text)
+    articles = []
+    for result in data['news_results']:
+        articles.append({
+            'title': result['title'],
+            'link': result['link'],
+            'date': result['date'],
+            'source': result['source']
+        })
+    return articles
+
+def scrape_article_text(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
+
+def generate_summary(text, api_key=None):
+    prompt = f"Please provide a short summary of the salient points for newsjacking ideation for this article: {text}"
+    response = openai.ChatCompletion.create(
+        model="gpt-4-0314",
+        messages=[
+            {"role": "system", "content": "You are an expert at summarizing articles for newsjacking ideation."},
+            {"role": "user", "content": prompt},
+        ],
+        max_tokens=500,
+        n=1,
+        stop=None,
+        temperature=0.5,
+        api_key=api_key if api_key else openai_key
+    )
+    summary = response['choices'][0]['message']['content']
+    return summary.strip()
+
+def generate_content(articles, summaries, api_key=None):
+    content = []
+    for i, summary in enumerate(summaries):
+        prompt = f"Based on the summary of article '{articles[i]['title']}', please provide a story title, description, and dataset sources for newsjacking ideation."
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are an expert at generating newsjacking ideas based on article summaries."},
+                {"role": "user", "content": f"{prompt}: {summary}"},
+            ],
+            max_tokens=2000,
+            n=1,
+            stop=None,
+            temperature=0.5,
+            api_key=api_key if api_key else openai_key
+        )
+        generated_content = response['choices'][0]['message']['content'].strip()
+        content.append({
+            'article_title': articles[i]['title'],
+            'input_summary': summary,
+            'generated_content': generated_content
+        })
+    return content
 def main():
     st.set_page_config(page_title='Newsjacking Ideation', layout='wide')
     st.title('Newsjacking Ideation App')
